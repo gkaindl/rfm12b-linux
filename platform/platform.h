b0f6ebae49fe695dc3af1214ba56dab64bf34c86
Georg Kaindl <gkaindl@mac.com>
2012-06-30 20:08:37 +0200

    put platform function declaration into their own header.

---
+++ platform/platform.h
@@ -0,0 +1,8 @@
+#if !defined(__PLATFORM_H__)
+
+static int platform_module_init(struct rfm12_data* dev_data);
+static int platform_irq_init(struct rfm12_data* dev_data);
+static int platform_irq_cleanup(struct rfm12_data* dev_data);
+static int platform_module_cleanup(struct rfm12_data* dev_data);
+
+#endif
